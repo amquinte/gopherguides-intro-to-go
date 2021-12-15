90d5df61d39e6a8b7de7c316711d27d830d99f5a
Anthony Quintero-Quiroga <anthony.quiroga@gmail.com>
2021-12-14 17:36:27 -0800

    Week06 Initial Commit
    Uploaded initial files and wrote complete tests for completed_product.go and errors.go.

---
+++ week06/completed_product_test.go
@@ -0,0 +1,61 @@
+package week06
+
+import (
+	"errors"
+	"testing"
+)
+
+// func Test_CP_IsValid(t *testing.T) {
+// 	e := -1
+// 	cp := CompletedProduct{Employee: Employee(e)}
+// 	act := cp.IsValid()
+// 	exp := ErrInvalidEmployee(e)
+// 	if errors.Is(act, exp) == false {
+// 		t.Fatalf("expect %v but got %v instead", exp, act)
+// 	}
+// 	fmt.Println("end of test")
+// }
+
+// func Test_CP_IsValid2(t *testing.T) {
+// 	e := 1
+// 	cp := CompletedProduct{Employee: Employee(e)}
+// 	act := cp.IsValid()
+// 	exp := ErrInvalidQuantity(0)
+// 	if errors.Is(act, exp) == false {
+// 		t.Fatalf("expect %v but got %v instead", exp, act)
+// 	}
+// 	fmt.Println("end of test")
+// }
+
+// func Test_CP_IsValid3(t *testing.T) {
+// 	cp := CompletedProduct{Product: Product{Quantity: 1, builtBy: Employee(1)}, Employee: 1}
+// 	act := cp.IsValid()
+
+// 	if act != nil {
+// 		t.Fatalf("expect nil but got %v instead", act)
+// 	}
+// 	fmt.Println("end of test")
+// }
+
+func Test_CP_TDT(t *testing.T) {
+	t.Parallel()
+	table := []struct {
+		name string
+		cp   CompletedProduct
+		err  error
+	}{
+		{name: "invalidEmployee", cp: CompletedProduct{Employee: -1}, err: ErrInvalidEmployee(-1)},
+		{name: "invalidQuantity", cp: CompletedProduct{Employee: 1}, err: ErrInvalidQuantity(0)},
+		{name: "validCP", cp: CompletedProduct{Product: Product{Quantity: 1, builtBy: Employee(1)}, Employee: 1}, err: nil},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			act := tt.cp.IsValid()
+
+			if errors.Is(tt.err, act) == false {
+				t.Fatalf("expect %v but got %v instead", tt.err, act)
+			}
+		})
+	}
+}
---
+++ week06/errors_test.go
@@ -0,0 +1,55 @@
+package week06
+
+import (
+	"testing"
+)
+
+func Test_Errors_ErrInvalidQuantity_Error(t *testing.T) {
+	t.Parallel()
+	err := ErrInvalidQuantity(0)
+	exp := "quantity must be greater than 0, got 0"
+	act := err.Error()
+	if act != exp {
+		t.Fatalf("expected %v but got %v instead", exp, act)
+	}
+}
+
+func Test_Errors_ErrProductNotBuilt_Error(t *testing.T) {
+	t.Parallel()
+	err := ErrProductNotBuilt("")
+	exp := ""
+	act := err.Error()
+	if act != exp {
+		t.Fatalf("expected %v but got %v instead", exp, act)
+	}
+}
+
+func Test_Errors_ErrInvalidEmployee_Error(t *testing.T) {
+	t.Parallel()
+	err := ErrInvalidEmployee(-1)
+	exp := "invalid employee number: -1"
+	act := err.Error()
+	if act != exp {
+		t.Fatalf("expected %v but got %v instead", exp, act)
+	}
+}
+
+func Test_Errors_ErrInvalidEmployeeCount_Error(t *testing.T) {
+	t.Parallel()
+	err := ErrInvalidEmployeeCount(-1)
+	exp := "invalid employee count: -1"
+	act := err.Error()
+	if act != exp {
+		t.Fatalf("expected %v but got %v instead", exp, act)
+	}
+}
+
+func Test_Erros_ErrManagerStopped_Error(t *testing.T) {
+	t.Parallel()
+	err := ErrManagerStopped{}
+	exp := "manager is stopped"
+	act := err.Error()
+	if act != exp {
+		t.Fatalf("expected %v but got %v instead", exp, act)
+	}
+}
